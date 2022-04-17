03bd12b24c131356b343ee11f33830973e7b1e76
SalamanderKip <200507@student.glu.nl>
2022-04-17 21:50:25 +0200

    af

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+
+Route::resource('posts', PostController::class)->middleware(['auth']);
+// Route::get('/posts', function () {
+//     return view('posts.index');
+// })->middleware(['auth'])->name('posts');
+
+// Route::get('/posts/create', function () {
+//     return view('posts.create');
+// })->middleware(['auth'])->name('posts');
